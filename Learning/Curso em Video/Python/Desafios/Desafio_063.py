a86711e84175692a894567e0500810bb113b5ac9
Carlos <138033782+FeedCarlos@users.noreply.github.com>
2026-02-24 21:14:25 -0300

    Update Challenger 063

--- Learning/Curso em Video/Python/Desafios/Desafio_063.py
+++ Learning/Curso em Video/Python/Desafios/Desafio_063.py
@@ -5,17 +5,17 @@ print('Sequência de Fibonacci')
 print('-'*25)
 
 n = int(input('Digite um número para vermos a sequência: '))
-ini = n
-ini = 1
-f_1 = ini - 1
-f_2 = ini - 2
-calc = f_1 + f_2
 count = 0
-final = 7
-while count != final:
-    print('{}'.format(ini))
-    if ini == 1:
-        ini += 1
-    else: 
-        ini = calc
+n2 = n - 1
+n3 = n - 2
+while count < 10:
+    print('1-{}'.format(n))
+    print('2-{}'.format(n2))
+    print('3-{}'.format(n3))
+    count += 1
+    n += 1
+    n2 += 1
+    n3 += 1
+    if n2 >= 0 and n3 >= 0:
+        n = (n2 + n3)
 print('FIM')
