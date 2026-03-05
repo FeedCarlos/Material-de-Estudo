a48a3bc36126eb7d9a1bd4b561d22b8d5894bec3
Carlos <138033782+FeedCarlos@users.noreply.github.com>
2026-03-05 20:07:59 -0300

    IDK

--- Learning/Curso em Video/Python/Desafios/Desafio_065.py
+++ Learning/Curso em Video/Python/Desafios/Desafio_065.py
@@ -1,24 +1,36 @@
 # Crie um programa que leia vários números inteiros pelo teclado. No final da execução, mostre a média entre todos os valores e qual foi o maior e o menor valor lido. O programa deve perguntar ao usuário se ele quer ou não continuar a digitar valores.
+from time import sleep
+print('-='*40)
+print('Digite alguns valores para descobrirmos o total e a média entre eles')
+print('-='*40)
+sleep(1)
+print('VAMOS COMEÇAR!')
+sleep(1)
+print('-='*35)
 
-num = int(input('Digite alguns valores para descobrirmos a média entre eles:'))
 maior = 0
 menor = 0
 count = 0
 total = 0
 media = 0
-prox = ''
-while prox == 'N':
-    prox = str(input('Quer continuar? [S/N]'))
-    if prox != 'N':
-        num = int(input('Digite outro:'))
-        soma = num + total
-        total = soma 
+prox = 'S'
+while prox != 'N':
+    num = int(input('Digite: '))
+    soma = num + total
+    total = soma  
+    if count == 0:
         maior = num
-        menor = num 
+        menor = num
+    else:
         if num > maior:
             maior = num
-        else:
+        if num < menor:
             menor = num
+    prox = str(input('Gostaria de continuar? [S/N]\n→ ')).upper()
+    if prox == 'N':
+        prox = 'N'
     count += 1
-    media = total / count
-print('Total: {}, Média {}, Maior {}, Menor {}'.format(total, media, maior, menor))
+media = total / count
+print('-='*40)
+print('O total foi {}\nA média {:.2f}\nO maior valor {}\nO menor valor {}'.format(total, media, maior, menor))
+print('-='*40)
