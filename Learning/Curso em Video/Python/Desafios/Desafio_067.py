05075cc3acec6e564cf48bcff897f6d0ba0fc432
Carlos <138033782+FeedCarlos@users.noreply.github.com>
2026-04-06 21:01:11 -0300

    Challenger 73.
    Challenger 73 of Python solved.

---
+++ Learning/Curso em Video/Python/Desafios/Desafio_067.py
@@ -0,0 +1,15 @@
+# Faça um programa que mostre a tabuada de vários números, um de cada vez, para cada valor digitado pelo usuário. O programa será interrompido quando o número solicitado for negativo
+print('Digite um vários númeors')
+while True:
+    n = int(input('Digite um número: '))
+    print('-'*30)
+    if n < 0:
+        break
+    else:
+        for c in range (1, 11):
+            m = c * n
+            print(f"{n} x {c} = {m}") 
+    print('-'*30)
+print("Fim")
+             
+
--- Learning/Curso em Video/Python/Mundo_3/Desafio_073.py
+++ Learning/Curso em Video/Python/Mundo_3/Desafio_073.py
@@ -1,7 +1,7 @@
 # Crie uma tupla preenchida com os 20 primeiros colocados da tabela do campeonato brasileiro de futebol, na ordem de colocação. Depois mostre:
 # A) Apenas os 5 primeiros colocados. - B) Os últimos 4 colocados da tabela - C) Uma lista com os times em ordem alfabética. - D) Em que posição na tabela está o time da Chapecoense
 
-brasileirao = ('Palmeiras', 'Athletico PR', 'São Paulo', 'Fluminense', 'Flamengo', 'Bahia', 'Coritiba', 'Grêmio', 'Vasco da Gama', 'Vitória', 'Corinthians', 'Internacional', 'Atlético-MG', 'Red Bull Bragantino', 'Chapecoense' 'Santos', 'Botafogo', 'Mirassol', 'Remo-PA', 'Cruzeiro')
+brasileirao = ('Palmeiras', 'Athletico PR', 'São Paulo', 'Fluminense', 'Flamengo', 'Bahia', 'Coritiba', 'Grêmio', 'Vasco da Gama', 'Vitória', 'Corinthians', 'Internacional', 'Atlético-MG', 'Red Bull Bragantino', 'Chapecoense', 'Santos', 'Botafogo', 'Mirassol', 'Remo-PA', 'Cruzeiro')
 print('-'*30)
 print('   TABELA DO BRASILEIRÃO')
 print('-'*30)
@@ -11,4 +11,26 @@ print(f'''1º {brasileirao[0]}
 3º {brasileirao[2]}
 4º {brasileirao[3]}
 5º {brasileirao[4]}\n''')
+print('')
+print('-'*30)
+print('Os Últimos 4 Colocados')
+print('-'*30)
+print('')
+print(f'''10º {brasileirao[-1]}
+9º {brasileirao[-2]}
+8º {brasileirao[-3]}
+7º {brasileirao[-4]}\n''')
+print('-'*30)
+print('   Em Ordem Alfabética')
+print('-'*30)
+print('')
+print(sorted(brasileirao))
+print('')
+print('-'*30)
+print('   A Posição do Chapecoense')
+print('-'*30)
+print('')
+chapecoense = brasileirao.index('Chapecoense')
+chapecoense += 1
+print(f'{chapecoense}º Chapecoense')
 print('')
