dfdfb2bda6aacfabf9c5e8ccae9495ce3a367b7c
Carlos <138033782+FeedCarlos@users.noreply.github.com>
2026-09-15 21:05:14 -0300

    New Challengers
    New task Modulation.

---
+++ Learning/Curso em Video/Python/Aulas/Aula_022.py
@@ -0,0 +1,19 @@
+# Modulariazação
+from uteis import numeros
+
+num = int(input("Digite um número: "))
+fat=numeros.fatorial(num)
+print(f"O fatorial de {num} é {fat}")
+
+'''
+Vantagens da modulariazação
+
+ - Organização do Código
+ - Facilidade na menutenção
+ - Ocultação de código detalhado
+ - Reutilização em outros projetos
+'''
+
+'''
+Pacote Uteis
+'''
